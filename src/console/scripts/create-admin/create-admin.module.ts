af1953cdcbcdd95fbd086948090e7e9b839c6ee0
Artem Grachov <grachov2195@gmail.com>
2025-12-04 23:28:18 +0200

    refactor: create admin script, CSA-82

--- src/console/scripts/create-admin/create-admin.module.ts
+++ src/console/scripts/create-admin/create-admin.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
 import { PasswordModule } from 'src/modules/password/password.module';
+import { ParametersModule } from 'src/console/modules/parameters/parameters.module';
 
 import { CreateAdminService } from './create-admin.service';
 import { PrismaService } from '../../../prisma.service';
 
 @Module({
-  imports: [ConfigModule.forRoot(), PasswordModule],
+  imports: [ConfigModule.forRoot(), ParametersModule, PasswordModule],
   providers: [PrismaService, CreateAdminService]
 })
 export class CreateAdminModule {}
